8e8fc5d96deacc7d5383bc65944cee1268312d83
fahamdev <faham.dev@gmail.com>
2022-06-05 19:21:59 +0600

    Fix unit test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello! Welcome to NestJS starter project!"', () => {
+      expect(appController.getHello()).toBe(
+        'Hello! Welcome to NestJS starter project!',
+      );
     });
   });
 });
