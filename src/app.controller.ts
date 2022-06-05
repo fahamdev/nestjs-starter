90892c4bb5fa833f4d405880c44eb0813ab07305
fahamdev <faham.dev@gmail.com>
2022-06-05 18:18:09 +0600

    Add controller-paths

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { API_ENDPOINTS } from 'src/common/utils/controller-paths';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: API_ENDPOINTS.BASE,
+  version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('base')
+  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
---
+++ src/common/utils/controller-paths.ts
@@ -0,0 +1,6 @@
+export const API_ENDPOINTS = {
+  BASE: 'base',
+  USERS: {
+    BASE: 'users',
+  },
+};
